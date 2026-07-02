a0b6d17a7e92e016927192bf6c1ecd97d72a7fa9
subho760 <nightshadow5g@gmail.com>
2026-07-02 07:21:15 +0530

    Implement JNI function to return a string

--- android/app/src/main/cpp/native-lib.cpp
+++ android/app/src/main/cpp/native-lib.cpp
@@ -1,1 +1,10 @@
+#include <jni.h>
+#include <string>
 
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_night_easyunitconverter_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello Core Native C++ Engine Functional";
+    return env->NewStringUTF(hello.c_str());
+}
